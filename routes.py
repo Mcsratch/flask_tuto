5640eb393d86bdf9bb490578fbef8292f0023a93
MC <leyogibear@yahoo.com>
2019-01-04 18:33:40 +0100

    heroku signup

--- routes.py
+++ routes.py
@@ -1,5 +1,5 @@
-from flask import Flask, render_template
-from models import db
+from flask import Flask, render_template, request
+from models import db, User
 from forms import SignupForm
 
 app = Flask(__name__)
@@ -17,9 +17,20 @@ def index():
 def about():
   return render_template("about.html")
 
-@app.route("/signup")
+@app.route("/signup", methods=["GET", "POST"])
 def signup():
-    form = SignupForm()
+  form = SignupForm()
+
+  if request.method == "POST":
+    if form.validate() == False:
+      return render_template('signup.html', form=form)
+    else:
+      newuser = User(form.first_name.data, form.last_name.data, form.email.data, form.password.data)
+      db.session.add(newuser)
+      db.session.commit()
+      return 'Success!'
+
+  elif request.method == "GET":
     return render_template('signup.html', form=form)
 
 if __name__ == "__main__":
